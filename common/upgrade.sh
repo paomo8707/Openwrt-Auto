79833b243de02df69ef680ba39ed4917af5063b8
datout <minitruu@gmail.com>
2026-04-18 17:18:28 +0800

    fix: adapt hyy autoupdate to Update tag releases

--- common/upgrade.sh
+++ common/upgrade.sh
@@ -11,6 +11,18 @@ function Diy_Part1() {
 		rm -rf "$HOME_PATH/package/autoupdate" "$HOME_PATH/package/luci-app-autoupdate"
 		[ -d "$tmpdir/autoupdate" ] && cp -r "$tmpdir/autoupdate" "$HOME_PATH/package/autoupdate"
 		cp -r "$tmpdir/luci-app-autoupdate" "$HOME_PATH/package/luci-app-autoupdate"
+
+		# 兼容当前 Openwrt-Auto 的发布规则：
+		# - 固件仍从用户自己的 Github Release 下载
+		# - 但 autoupdate 的云端查询不再固定走 AutoUpdate/latest
+		# - 改为根据当前板型读取 Update-${TARGET_BOARD} 这个 tag
+		if [[ -f "$HOME_PATH/package/autoupdate/files/bin/autoupdate" ]]; then
+			sed -i 's#Github_Release="${Github}/releases/download/AutoUpdate"#Github_Release="${Github}/releases/download/${UPDATE_TAG:-Update-${TARGET_BOARD}}"#' \
+				"$HOME_PATH/package/autoupdate/files/bin/autoupdate"
+			sed -i 's#Github_API="https://api.github.com/repos/${Firmware_Author}/releases/latest"#Github_API="https://api.github.com/repos/${Firmware_Author}/releases/tags/${UPDATE_TAG:-Update-${TARGET_BOARD}}"#' \
+				"$HOME_PATH/package/autoupdate/files/bin/autoupdate"
+		fi
+
 		rm -rf "$tmpdir"
 		if ! grep -q "luci-app-autoupdate" "${HOME_PATH}/include/target.mk"; then
 			sed -i 's?DEFAULT_PACKAGES:=?DEFAULT_PACKAGES:=luci-app-autoupdate autoupdate luci-app-ttyd ?g' ${HOME_PATH}/include/target.mk
@@ -26,13 +38,24 @@ function Diy_Part1() {
 function Diy_Part2() {
 	export UPDATE_TAG="Update-${TARGET_BOARD}"
 	export FILESETC_UPDATE="${HOME_PATH}/package/base-files/files/etc/openwrt_update"
+	export FILESETC_AUTOUPDATE_DEFAULT="${HOME_PATH}/package/base-files/files/etc/autoupdate/default"
 	export GITHUB_PROXY="https://ghfast.top"
 	export RELEASE_DOWNLOAD="\$GITHUB_LINK/releases/download/${UPDATE_TAG}"
 	export GITHUB_RELEASE="${GITHUB_LINK}/releases/tag/${UPDATE_TAG}"
-        if [[ ! -f "$LINSHI_COMMON/autoupdate/replace" ]]; then
+	export AUTOUPDATE_FLAG="Full"
+	export OP_VERSION="R${LUCI_EDITION}-${UPGRADE_DATE}"
+	export OP_AUTHOR="$(echo "${REPO_URL#https://github.com/}" | cut -d/ -f1)"
+	export OP_REPO="$(basename "${REPO_URL}")"
+	export OP_BRANCH="${REPO_BRANCH}"
+	export Author="$(echo "${GITHUB_LINK#https://github.com/}" | cut -d/ -f1)"
+	export Github="${GITHUB_LINK}"
+	export Log_Path="/tmp"
+
+	if [[ ! -f "$LINSHI_COMMON/autoupdate/replace" ]]; then
 		echo -e "\n\033[0;31m缺少autoupdate/replace文件\033[0m"
-   		exit 1
-  	fi
+		exit 1
+	fi
+
 	if [[ "${TARGET_PROFILE}" == *"k3"* ]]; then
 		export TARGET_PROFILE_ER="phicomm-k3"
 	elif [[ "${TARGET_PROFILE}" == *"k2p"* ]]; then
@@ -41,21 +64,21 @@ function Diy_Part2() {
 		export TARGET_PROFILE_ER="xiaomi_mir3g-v2"
 	elif [[ "$TARGET_PROFILE" == *xiaomi* && "$TARGET_PROFILE" == *3g* ]]; then
 		export TARGET_PROFILE_ER="xiaomi_mir3g"
- 	elif [[ "$TARGET_PROFILE" == *xiaomi* && "$TARGET_PROFILE" == *3* && "$TARGET_PROFILE" == *pro* ]]; then
+	elif [[ "$TARGET_PROFILE" == *xiaomi* && "$TARGET_PROFILE" == *3* && "$TARGET_PROFILE" == *pro* ]]; then
 		export TARGET_PROFILE_ER="xiaomi_mi3pro"
 	else
 		export TARGET_PROFILE_ER="${TARGET_PROFILE}"
 	fi
-	
+
 	case "${TARGET_BOARD}" in
-	ramips | reltek | ath* | ipq* | bmips | kirkwood | mediatek |bcm4908 |gemini |lantiq |layerscape |qualcommax |qualcommbe |siflower |silicon)
+	ramips | reltek | ath* | ipq* | bmips | kirkwood | mediatek | bcm4908 | gemini | lantiq | layerscape | qualcommax | qualcommbe | siflower | silicon)
 		export FIRMWARE_SUFFIX=".bin"
 		export AUTOBUILD_FIRMWARE="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
 	;;
- 	bcm47xx)
-          	if echo "$TARGET_PROFILE" | grep -Eq 'asus'; then
+	bcm47xx)
+		if echo "$TARGET_PROFILE" | grep -Eq 'asus'; then
 			export FIRMWARE_SUFFIX=".trx"
-             	elif echo "$TARGET_PROFILE" | grep -Eq 'netgear'; then
+		elif echo "$TARGET_PROFILE" | grep -Eq 'netgear'; then
 			export FIRMWARE_SUFFIX=".chk"
 		else
 			export FIRMWARE_SUFFIX=".bin"
@@ -67,7 +90,7 @@ function Diy_Part2() {
 		export AUTOBUILD_FIRMWARE_UEFI="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
 		export AUTOBUILD_FIRMWARE="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
 	;;
-	rockchip | bcm27xx | mxs | sunxi | zynq |loongarch64 |omap |sifiveu |tegra |amlogic)
+	rockchip | bcm27xx | mxs | sunxi | zynq | loongarch64 | omap | sifiveu | tegra | amlogic)
 		export FIRMWARE_SUFFIX=".img.gz"
 		export AUTOBUILD_FIRMWARE="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
 	;;
@@ -76,11 +99,11 @@ function Diy_Part2() {
 		export AUTOBUILD_FIRMWARE="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
 	;;
 	bcm53xx)
- 		if echo "$TARGET_PROFILE" | grep -Eq 'mr32|tplink|dlink'; then
+		if echo "$TARGET_PROFILE" | grep -Eq 'mr32|tplink|dlink'; then
 			export FIRMWARE_SUFFIX=".bin"
-     		elif echo "$TARGET_PROFILE" | grep -Eq 'luxul'; then
+		elif echo "$TARGET_PROFILE" | grep -Eq 'luxul'; then
 			export FIRMWARE_SUFFIX=".lxl"
-        	elif echo "$TARGET_PROFILE" | grep -Eq 'netgear'; then
+		elif echo "$TARGET_PROFILE" | grep -Eq 'netgear'; then
 			export FIRMWARE_SUFFIX=".chk"
 		else
 			export FIRMWARE_SUFFIX=".trx"
@@ -96,47 +119,78 @@ function Diy_Part2() {
 		export AUTOBUILD_FIRMWARE="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
 	;;
 	esac
-	
+
 	export FIRMWARE_VERSION="${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
+	export TARGET_PROFILE_AUTOUPDATE="${TARGET_PROFILE_ER}"
+	export BASE_AUTOBUILD_PREFIX="AutoBuild-${OP_REPO}-${TARGET_PROFILE_AUTOUPDATE}-${OP_VERSION}"
 
 	if [[ "${TARGET_BOARD}" == "x86" ]]; then
-   		BOOT_TYPE="legacy"
- 		echo "AUTOBUILD_FIRMWARE_UEFI=${AUTOBUILD_FIRMWARE_UEFI}-uefi" >> ${GITHUB_ENV}
-		echo "AUTOBUILD_FIRMWARE=${AUTOBUILD_FIRMWARE}-${BOOT_TYPE}" >> ${GITHUB_ENV}
+		BOOT_TYPE="legacy"
+		export AUTOBUILD_FIRMWARE_UEFI="${BASE_AUTOBUILD_PREFIX}-uefi-${AUTOUPDATE_FLAG}"
+		export AUTOBUILD_FIRMWARE="${BASE_AUTOBUILD_PREFIX}-${BOOT_TYPE}-${AUTOUPDATE_FLAG}"
+		echo "AUTOBUILD_FIRMWARE_UEFI=${AUTOBUILD_FIRMWARE_UEFI}" >> ${GITHUB_ENV}
+		echo "AUTOBUILD_FIRMWARE=${AUTOBUILD_FIRMWARE}" >> ${GITHUB_ENV}
 	elif [[ "${FIRMWARE_SUFFIX}" == ".img.gz" ]]; then
-   		BOOT_TYPE="legacy"
-		echo "AUTOBUILD_FIRMWARE=${AUTOBUILD_FIRMWARE}-${BOOT_TYPE}" >> ${GITHUB_ENV}
+		BOOT_TYPE="legacy"
+		export AUTOBUILD_FIRMWARE="${BASE_AUTOBUILD_PREFIX}-${BOOT_TYPE}-${AUTOUPDATE_FLAG}"
+		echo "AUTOBUILD_FIRMWARE=${AUTOBUILD_FIRMWARE}" >> ${GITHUB_ENV}
 	else
- 		BOOT_TYPE="sysupgrade"
-		echo "AUTOBUILD_FIRMWARE=${AUTOBUILD_FIRMWARE}-${BOOT_TYPE}" >> ${GITHUB_ENV}
+		BOOT_TYPE="sysupgrade"
+		export AUTOBUILD_FIRMWARE="${BASE_AUTOBUILD_PREFIX}-${BOOT_TYPE}-${AUTOUPDATE_FLAG}"
+		echo "AUTOBUILD_FIRMWARE=${AUTOBUILD_FIRMWARE}" >> ${GITHUB_ENV}
 	fi
 
- 	echo "UPDATE_TAG=${UPDATE_TAG}" >> ${GITHUB_ENV}
+	echo "UPDATE_TAG=${UPDATE_TAG}" >> ${GITHUB_ENV}
 	echo "FIRMWARE_SUFFIX=${FIRMWARE_SUFFIX}" >> ${GITHUB_ENV}
 	echo "AUTOUPDATE_VERSION=${AUTOUPDATE_VERSION}" >> ${GITHUB_ENV}
 	echo "FIRMWARE_VERSION=${FIRMWARE_VERSION}" >> ${GITHUB_ENV}
 	echo "GITHUB_RELEASE=${GITHUB_RELEASE}" >> ${GITHUB_ENV}
+	echo "OP_VERSION=${OP_VERSION}" >> ${GITHUB_ENV}
+	echo "TARGET_FLAG=${AUTOUPDATE_FLAG}" >> ${GITHUB_ENV}
 
-
-	# 写入openwrt_update文件
+	# 写入 openwrt_update / autoupdate 默认环境文件
+	mkdir -p "$(dirname "${FILESETC_UPDATE}")" "$(dirname "${FILESETC_AUTOUPDATE_DEFAULT}")"
 	install -m 0755 /dev/null "${FILESETC_UPDATE}"
+	install -m 0755 /dev/null "${FILESETC_AUTOUPDATE_DEFAULT}"
+
 	echo "GITHUB_LINK=\"${GITHUB_LINK}\"" >> ${FILESETC_UPDATE}
- 	echo "FIRMWARE_VERSION=\"${FIRMWARE_VERSION}\"" >> ${FILESETC_UPDATE}
- 	echo "LUCI_EDITION=\"${LUCI_EDITION}\"" >> ${FILESETC_UPDATE}
- 	echo "SOURCE=\"${SOURCE}\"" >> ${FILESETC_UPDATE}
-   	echo "DEVICE_MODEL=\"${TARGET_PROFILE_ER}\"" >> ${FILESETC_UPDATE}
- 	echo "FIRMWARE_SUFFIX=\"${FIRMWARE_SUFFIX}\"" >> ${FILESETC_UPDATE}
- 	echo "TARGET_BOARD=\"${TARGET_BOARD}\"" >> ${FILESETC_UPDATE}
- 	echo "GITHUB_PROXY=\"${GITHUB_PROXY}\"" >> ${FILESETC_UPDATE}
- 	echo "RELEASE_DOWNLOAD=\"${RELEASE_DOWNLOAD}\"" >> ${FILESETC_UPDATE}
+	echo "FIRMWARE_VERSION=\"${FIRMWARE_VERSION}\"" >> ${FILESETC_UPDATE}
+	echo "LUCI_EDITION=\"${LUCI_EDITION}\"" >> ${FILESETC_UPDATE}
+	echo "SOURCE=\"${SOURCE}\"" >> ${FILESETC_UPDATE}
+	echo "DEVICE_MODEL=\"${TARGET_PROFILE_ER}\"" >> ${FILESETC_UPDATE}
+	echo "FIRMWARE_SUFFIX=\"${FIRMWARE_SUFFIX}\"" >> ${FILESETC_UPDATE}
+	echo "TARGET_BOARD=\"${TARGET_BOARD}\"" >> ${FILESETC_UPDATE}
+	echo "GITHUB_PROXY=\"${GITHUB_PROXY}\"" >> ${FILESETC_UPDATE}
+	echo "RELEASE_DOWNLOAD=\"${RELEASE_DOWNLOAD}\"" >> ${FILESETC_UPDATE}
+	echo "UPDATE_TAG=\"${UPDATE_TAG}\"" >> ${FILESETC_UPDATE}
+	echo "Author=\"${Author}\"" >> ${FILESETC_UPDATE}
+	echo "Github=\"${Github}\"" >> ${FILESETC_UPDATE}
+	echo "TARGET_PROFILE=\"${TARGET_PROFILE_AUTOUPDATE}\"" >> ${FILESETC_UPDATE}
+	echo "TARGET_FLAG=\"${AUTOUPDATE_FLAG}\"" >> ${FILESETC_UPDATE}
+	echo "OP_VERSION=\"${OP_VERSION}\"" >> ${FILESETC_UPDATE}
+	echo "OP_AUTHOR=\"${OP_AUTHOR}\"" >> ${FILESETC_UPDATE}
+	echo "OP_BRANCH=\"${OP_BRANCH}\"" >> ${FILESETC_UPDATE}
+	echo "OP_REPO=\"${OP_REPO}\"" >> ${FILESETC_UPDATE}
+	echo "Log_Path=\"${Log_Path}\"" >> ${FILESETC_UPDATE}
 	cat "$LINSHI_COMMON/autoupdate/replace" >> ${FILESETC_UPDATE}
 
- 	# 写入del_assets文件
+	echo "Author=\"${Author}\"" >> ${FILESETC_AUTOUPDATE_DEFAULT}
+	echo "Github=\"${Github}\"" >> ${FILESETC_AUTOUPDATE_DEFAULT}
+	echo "TARGET_PROFILE=\"${TARGET_PROFILE_AUTOUPDATE}\"" >> ${FILESETC_AUTOUPDATE_DEFAULT}
+	echo "TARGET_FLAG=\"${AUTOUPDATE_FLAG}\"" >> ${FILESETC_AUTOUPDATE_DEFAULT}
+	echo "OP_VERSION=\"${OP_VERSION}\"" >> ${FILESETC_AUTOUPDATE_DEFAULT}
+	echo "OP_AUTHOR=\"${OP_AUTHOR}\"" >> ${FILESETC_AUTOUPDATE_DEFAULT}
+	echo "OP_BRANCH=\"${OP_BRANCH}\"" >> ${FILESETC_AUTOUPDATE_DEFAULT}
+	echo "OP_REPO=\"${OP_REPO}\"" >> ${FILESETC_AUTOUPDATE_DEFAULT}
+	echo "UPDATE_TAG=\"${UPDATE_TAG}\"" >> ${FILESETC_AUTOUPDATE_DEFAULT}
+	echo "Log_Path=\"${Log_Path}\"" >> ${FILESETC_AUTOUPDATE_DEFAULT}
+
+	# 写入del_assets文件
 	install -m 0755 /dev/null "${GITHUB_WORKSPACE}/del_assets"
-  	echo "UPDATE_TAG=\"${UPDATE_TAG}\"" >> "${GITHUB_WORKSPACE}/del_assets"
-  	echo "BOOT_TYPE=\"${BOOT_TYPE}\"" >> "${GITHUB_WORKSPACE}/del_assets"
+	echo "UPDATE_TAG=\"${UPDATE_TAG}\"" >> "${GITHUB_WORKSPACE}/del_assets"
+	echo "BOOT_TYPE=\"${BOOT_TYPE}\"" >> "${GITHUB_WORKSPACE}/del_assets"
 	echo "FIRMWARE_SUFFIX=\"${FIRMWARE_SUFFIX}\"" >> "${GITHUB_WORKSPACE}/del_assets"
- 	echo "FIRMWARE_PROFILEER=\"${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}\"" >> "${GITHUB_WORKSPACE}/del_assets"
+	echo "FIRMWARE_PROFILEER=\"AutoBuild-${OP_REPO}-${TARGET_PROFILE_AUTOUPDATE}-R${LUCI_EDITION}\"" >> "${GITHUB_WORKSPACE}/del_assets"
 }
 
 function Diy_Part3() {
